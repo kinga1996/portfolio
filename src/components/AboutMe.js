a5fe5289b0a7b64045566984c134fd8edb9d01ab
Kinga Kalisz <kingakalisz@gmail.com>
2021-03-12 17:55:35 +0100

    commit before responsible

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -22,9 +22,10 @@ const AboutMe = () => {
             <div className="about-info">
               <h2>Hi!</h2>
               <p>
-                In id nulla magna. Nullam posuere fermentum mattis. Nunc id dui
-                at sapien faucibus fermentum ut vel diam. Nullam tempus, nunc id
-                efficitur sagittis, urna est ultricies eros, ac porta sem turpis
+              I am ambitious and always willing to self-development person,
+              whose one of the main hobby is computer science (since primary
+              school, the program Logomocja and websites created in HTML based on tables :) ).
+              <br></br>Since June 2020, I am developing my skills to become a really great Front-end Developer
               </p>
               <div className="about-contact">
                   {state.map((info) => (
@@ -35,6 +36,12 @@ const AboutMe = () => {
                   ))}
                 </div>
                 <a href = {Pdf} target = "_blank">See my resume</a>
+              <div className="can-do">
+                <h6>My skills:</h6>
+                <ul>
+                  <li>HTML5, CSS3, JavaScript</li>
+                </ul>
+              </div>
             </div>
     </div>
   );
